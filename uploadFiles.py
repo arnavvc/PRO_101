46f02f19c9d9524fce3b0a2730e9cd194915a149
arnavvc <72585981+arnavvc@users.noreply.github.com>
2021-11-14 11:24:38 -0800

    Add files via upload

---
+++ uploadFiles.py
@@ -0,0 +1,36 @@
+import dropbox
+import os
+from dropbox.files import WriteMode
+
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, file_from, file_to):
+        dbx = dropbox.Dropbox(self.access_token)
+
+        for root, dirs, files in os.walk(file_from):
+
+            for filename in files:
+                local_path = os.path.join(root, filename)
+
+                relative_path = os.path.relpath(local_path, file_from)
+                dropbox_path = os.path.join(file_to, relative_path)
+
+                with open(local_path, 'rb') as f:
+                    #dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))
+                    dbx.files_upload(f.read(), dropbox_path, mode=dropbox.files.WriteMode.overwrite)
+
+
+def main():
+    access_token = 'sl.A4jThLUNHyRu00PGTMGlnoDkg0lgFDgeLCMfi1WUepzgTa3DvKZTyXrmQShcXy9odBwfxt19w2dVrNycYcszGFni3Flgds2NJ-gCBXmqUkBQxxQDj5lKFUG54eDT_JnL0pPvlpg'
+    transferData = TransferData(access_token)
+
+    file_from = str(input("Enter the file path to transfer: ")) #not full path, just the name and extension is fine
+    file_to = "/BackUp2C101/" + (file_from) #input("Enter the full path to upload to dropbox: ")
+
+    transferData.upload_file(file_from, file_to)
+    print("file has been moved !!!")
+
+
+main()
